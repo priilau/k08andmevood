fb711080f1bc7ec8b11bfcf73f8cafb6aea15c02
Priit Laupa <priilau@users.noreply.github.com>
2019-04-16 17:35:58 +0300

    Lisatud kodutöö

---
+++ Voog.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.nio.file.*;
+import java.util.stream.*;
+import java.io.*;
+
+public class Voog{
+  public static void main(String[] args)throws Exception{
+    PrintWriter kirjutaja = new PrintWriter(new FileWriter("valjund.txt"));
+
+    Files.readAllLines(Paths.get("sisend.txt")).
+      stream().
+      filter(s -> Integer.parseInt(s.split(",")[2]) > 160).
+      sorted().
+      map(s -> "Õpilane " + s.split(",")[0] + ", pikkusega " + s.split(",")[2] + " cm, on pikem kui 160 cm").
+      collect(Collectors.toList()).
+      forEach(kirjutaja::println)
+      ;
+    kirjutaja.close();
+  }
+}
+
+/*
+Sisend:
+
+Juku,M,176
+Madis,M,170
+Liisa,N,166
+Juhan,M,164
+Malle,N,158
+Kati,N,160
+Kalle,M,169
+Grete,N,149
+Maali,N,155
+Meelis,M,171
+
+Väljund:
+
+Õpilane Juhan, pikkusega 164 cm, on pikem kui 160 cm
+Õpilane Juku, pikkusega 176 cm, on pikem kui 160 cm
+Õpilane Kalle, pikkusega 169 cm, on pikem kui 160 cm
+Õpilane Liisa, pikkusega 166 cm, on pikem kui 160 cm
+Õpilane Madis, pikkusega 170 cm, on pikem kui 160 cm
+Õpilane Meelis, pikkusega 171 cm, on pikem kui 160 cm
+
+*/
